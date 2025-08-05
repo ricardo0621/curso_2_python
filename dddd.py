c7b17423deab50e4616c52d9c6a96a0308829a4d
ricardo0621 <ricardo_0621@hotmail.com>
2025-08-05 11:55:59 -0500

    esta es una prueba

---
+++ dddd.py
@@ -0,0 +1,11 @@
+print('*** Sistema de Reserva de Hoteles ***')
+cliente = 'Laura _martinez'
+estancia_dias = 5
+tarifa_dias = 1200.2
+vista_mar = True
+
+print('Cliente: ', cliente)
+print('estancias dias: ' + str(estancia_dias))
+print(vista_mar)
+
+
